877c97d80e438b32d871dc491900a3ce420ca99a
derekfkw <derekfkw@yahoo.com.hk>
2022-04-05 19:31:14 +0800

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://ddb46e88.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
